84e9d86ce39d484dd1b1b9165c4ab1c9c3fa3558
Girfanova Yuliya <yuliyagiryul@gmail.com>
2024-03-26 22:34:10 +0500

    edit header link admin.php

--- header.php
+++ header.php
@@ -14,31 +14,20 @@
                         </ul>
                     </li>
                     <li><a href="about-us.php" class="menu_link">О нас</a></li>
-                    <li><a href="portfolio.php" class="menu_link">Портфолио</a></li>
-                    <!--<li><a href="promotions.php" class="menu_link">Акции</a></li>-->
-                    
+                    <li><a href="portfolio.php" class="menu_link">Портфолио</a></li>                    
                 </ul>
                 <?php
                 if ($_SESSION["auth"]==true) 
-                echo "<button onclick=\"window.location.href = 'lk.php'\" class='header_lk_btn btn'>Личный кабинет</button>";
-                else echo '<button class="header_authorization_btn btn popup_autor">Войти/Авторизоваться</button>';
+                echo "<button onclick=\"window.location.href = 'admin.php'\" class='header_lk_btn btn'>Админ-панель</button>";
                 ?>
 
             </nav>
             <div id="menu-burg" class='black-menu-burg'><span></span></div>
             <?php
-                if ($_SESSION["auth"]==true) {
-                    require_once("connect_db.php");
-                    mysqli_select_db($link,"aphrodite") or die("Ошибка подключения к базе данных");
-                    $query = "SELECT name FROM users WHERE id = ".$_SESSION['user_id'];
-                    $result = mysqli_query($link, $query);
-                    $user =  mysqli_fetch_array($result);
-                    
-                    echo " <a class='lk-open' href='lk.php'><div class='profile-circle' id = \"auth_circle\" class=\"popup_autor\" 
-                    style='width:35px; height:35px;font-size:1.5em; color:black; background-color:rgba(0,0,0,0); border:solid;'>".mb_substr($user['name'], 0, 1)."</div></a>";
+                if ($_SESSION["auth"]==true) {                    
+                    echo " <a class='lk-open' href='admin.php'><div class='profile-circle' id = \"auth_circle\" class=\"popup_autor\" 
+                    style='width:35px; height:35px;font-size:1.5em; color:black; background-color:rgba(0,0,0,0); border:solid;'>А</div></a>";
                 }
-                else echo "<div class=\"authorization\"><img src=\"Resources/авторизация-black.png\" id = \"auth\" class=\"popup_autor\" alt=\"авторизация\"
-                width=\"30\"></div>";
                 
             ?>
            
