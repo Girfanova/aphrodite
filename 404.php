dd8eb7fd71f033d79b239caef9af99e30dce4ab3
Girfanova Yuliya <yuliyagiryul@gmail.com>
2024-04-26 18:49:03 +0500

    стиль 404

--- 404.php
+++ 404.php
@@ -5,44 +5,6 @@
 <?php require_once('head.html') ?>
 </head>
 <?php require_once('header.php') ?>
-<style>
-
-.img-404{
-width:20vh;
-}
-
-.text-404{
-width: 100%;
-height: 100%;
-align-items: center;
-display: flex;
-justify-content: center;
-flex-direction: column;
-/* font-family: var(--first-title-font); */
-font-size: 5em;
-}
-.text-404 p, .text-404 a{
-    font-size: 0.3em;
-    margin: 1%;
-    font-family: var(--text-font);
-    text-align: center;
-}
-.container-404{
-    display: flex;
-    height: 100vh;
-    background-color: #ffeac35e;
-}
-
-.home{
-    color:var(--first-color);
-    text-decoration: underline;
-}
-.home:hover{
-    color: var(--third-color);
-}
-
-</style>
-
 <body>
     <div class="container-404">
         <div class="text-404">
